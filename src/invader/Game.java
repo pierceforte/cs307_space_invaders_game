e0d4525dd60d5fe004cd69696746abeb43dc578f
Pierce Forte <phf7@duke.edu>
2020-02-06 03:01:41 -0500

    Cleaned up Game and GameTest classes by eliminating duplicate code

--- src/invader/Game.java
+++ src/invader/Game.java
@@ -20,7 +20,6 @@ import javafx.util.Duration;
 public class Game extends Application {
     public static final String TITLE = "Space Invaders";
     public static final String WINNING_MESSAGE = "WINNER!";
-    //public static final int SIZE = 400;
     public static final int GAME_WIDTH = 400;
     public static final int GAME_HEIGHT = 600;
     public static final int SCENE_WIDTH = 400;
@@ -28,11 +27,6 @@ public class Game extends Application {
     public static final int FRAMES_PER_SECOND = 60;
     public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
     public static final Paint BACKGROUND = Color.BLACK;
-    public static final int MOVER_SPEED = 5;
-    public static final int BLOCK_SIZE = 30;
-    public static final int BLOCK_MIN_SPEED = 10;
-    public static final int BLOCK_MAX_SPEED = 100;
-    public static final int BLOCK_SPEEDUP_FACTOR = 2;
 
 
     // some things we need to remember during our game
@@ -40,9 +34,7 @@ public class Game extends Application {
     private Timeline myAnimation;
 
     private double gameTimer = 0;
-
     private Level level1;
-
     private Group root;
     /**
      * Initialize what will be displayed and how it will be updated.
@@ -62,24 +54,17 @@ public class Game extends Application {
         myAnimation.play();
     }
 
-    public void fire() {
-        level1.attemptSpaceshipFire(root, gameTimer);
-    }
-
-
     // Create the game's "scene": what shapes will be in the game and their starting properties
     public Scene setupScene(int width, int height, Paint background) {
         // create one top level collection to organize the things in the scene
         root = new Group();
-        // make some shapes, set their properties, and add them to the scene
-
 
         // create a place to see the shapes
         myScene = new Scene(root, width, height, background);
 
+        // create a level
         level1 = new Level("resources/level_files/level_01.txt", 1);
         level1.addEnemiesAndSpaceshipToScene(root);
-
 
         // respond to input
         myScene.setOnKeyPressed(e -> handleKeyInput(e.getCode()));
@@ -92,27 +77,15 @@ public class Game extends Application {
 
     // Change properties of shapes to animate them
     void step (double elapsedTime) {
-
         gameTimer += elapsedTime;
-
         level1.handleEntitiesAndLasers(root, gameTimer, elapsedTime);
-        // get internal values of other classes
-
-        // update attributes
-
-        // check for collisions
-
-        // TODO: check for win and, if true, pause the animation
     }
 
     // What to do each time a key is pressed
     private void handleKeyInput (KeyCode code) {
-        // move player
-
         if (code == KeyCode.RIGHT) {
             //moverShape.setX(moverShape.getX() + MOVER_SPEED);
             level1.moveSpaceship(true);
-
         }
         else if (code == KeyCode.LEFT) {
             //moverShape.setX(moverShape.getX() - MOVER_SPEED);
@@ -137,7 +110,6 @@ public class Game extends Application {
             level1.addEnemiesAndSpaceshipToScene(root);
         }
     }
-
 
     /**
      * Start the program.
--- test/invader/GameTest.java
+++ test/invader/GameTest.java
@@ -6,30 +6,24 @@ import invader.projectile.Laser;
 import javafx.application.Platform;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.concurrent.TimeUnit;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GameTest extends DukeApplicationTest {
+    private final String ENEMY_ABOVE_SPACESHIP = "#enemy31";
     private final Game myGame = new Game();
-    private Scene myScene;
 
+    private Scene myScene;
     private Spaceship mySpaceship;
     private List<List<Enemy>> myEnemies = new ArrayList<>();
     private Enemy myEnemy31;
-    private Enemy myEnemy;
     private Laser mySpaceshipLaser;
-
 
     @Override
     public void start (Stage stage) {
@@ -39,53 +33,52 @@ public class GameTest extends DukeApplicationTest {
         stage.show();
 
         // find individual items within game by ID (must have been set in your code using setID())
-//        press(myScene, KeyCode.SPACE);
         mySpaceship = lookup("#spaceship").query();
-        //myEnemy = lookup("#enemy").query();
-        for (int i = 0; i < 4; i++) {
+        for (int row = 0; row < 4; row++) {
             myEnemies.add(new ArrayList<>());
-            for (int j = 0; j < Level.ENEMIES_PER_ROW; j++) {
-                myEnemies.get(i).add(lookup("#enemy" + (j + i*Level.ENEMIES_PER_ROW)).query());
+            for (int col = 0; col < Level.ENEMIES_PER_ROW; col++) {
+                myEnemies.get(row).add(lookup("#enemy" + (col + row*Level.ENEMIES_PER_ROW)).query());
             }
         }
-        myEnemy31 = lookup("#enemy31").query();
-        //press(myScene, KeyCode.SPACE);
-        myGame.fire();
-        sleep(1, TimeUnit.SECONDS);
-        mySpaceshipLaser = lookup("#spaceshipLaser0").query();
-        //mySpaceshipLaser = new Laser(0,0,false);
-        myEnemy = lookup("#enemy0").query();
+        // when a laser is fired from the spaceship's default position, it will hit enemy31
+        myEnemy31 = lookup(ENEMY_ABOVE_SPACESHIP).query();
+        press(myScene, KeyCode.SPACE);
+        // need to wait for scene to update after key press in application thread
+        Platform.runLater(() -> mySpaceshipLaser = lookup("#spaceshipLaser0").query());
     }
 
     @Test
     public void testSpaceshipLaserDisappearsWhenOutOfBounds() {
+        // need to include a run later to modify application thread from here
         Platform.runLater(() -> {
-            // check if laser and enemy are on scene before collision
-            assertEquals(true, myGame.getRoot().getChildren().contains(mySpaceshipLaser));
+            // check if laser is on scene before being out of bounds
+            assertTrue(isNodeInMyScene(mySpaceshipLaser));
             // position the laser one step prior to being out of bounds
             mySpaceshipLaser.setY(Game.GAME_HEIGHT - 20 + Laser.Y_SPEED*Game.SECOND_DELAY);
+            // step so laser is out of bounds
             myGame.step(Game.SECOND_DELAY);
-            // check if both enemy31 and the laser have been removed from scene upon collision
-            assertEquals(false, myGame.getRoot().getChildren().contains(mySpaceshipLaser));
+            // check if the laser has been removed from scene upon being out of bounds
+            assertFalse(isNodeInMyScene(mySpaceshipLaser));
         });
     }
 
     @Test
     public void testLaserCollisionWithEnemy() {
+        // need to include a run later to modify application thread from here
         Platform.runLater(() -> {
             // check if laser and enemy are on scene before collision
-            assertEquals(true, myGame.getRoot().getChildren().contains(myEnemy31));
-            assertEquals(true, myGame.getRoot().getChildren().contains(mySpaceshipLaser));
+            assertTrue(isNodeInMyScene(myEnemy31));
+            assertTrue(isNodeInMyScene(mySpaceshipLaser));
             // position the laser one step prior to hitting enemy31
             mySpaceshipLaser.setX(myEnemy31.getX());
             mySpaceshipLaser.setY(myEnemy31.getY() + 9.5*Laser.Y_SPEED*Game.SECOND_DELAY);
+            // step to initiate collision
             myGame.step(Game.SECOND_DELAY);
             // check if both enemy31 and the laser have been removed from scene upon collision
-            assertEquals(false, myGame.getRoot().getChildren().contains(myEnemy31));
-            assertEquals(false, myGame.getRoot().getChildren().contains(mySpaceshipLaser));
+            assertFalse(isNodeInMyScene(myEnemy31));
+            assertFalse(isNodeInMyScene(mySpaceshipLaser));
         });
     }
-
 
     @Test
     public void testSpaceshipInitialPosition () {
@@ -96,61 +89,50 @@ public class GameTest extends DukeApplicationTest {
     }
 
     @Test
-    public void testSpaceshipMove () {
-        // move it right one step by "pressing" the left arrow
-        sleep(1, TimeUnit.SECONDS);    // PAUSE: not typically recommended in tests
-        press(myScene, KeyCode.RIGHT);
-        sleep(1, TimeUnit.SECONDS);    // PAUSE: but useful when debugging to verify what is happening
-        // then check its position has changed properly
-        assertEquals(Spaceship.DEFAULT_X_POS + mySpaceship.X_SPEED_ON_KEY_PRESS, mySpaceship.getX());
-
-        // reset spaceship position
-        mySpaceship.setX(Spaceship.DEFAULT_X_POS);
-        // move it left one step by "pressing" the left arrow
-        sleep(1, TimeUnit.SECONDS);    // PAUSE: not typically recommended in tests
-        press(myScene, KeyCode.LEFT);
-        sleep(1, TimeUnit.SECONDS);    // PAUSE: but useful when debugging to verify what is happening
-        // then check its position has changed properly
-        assertEquals(Spaceship.DEFAULT_X_POS - mySpaceship.X_SPEED_ON_KEY_PRESS, mySpaceship.getX());
+    public void testSpaceshipMoveLeftAndRight () {
+        // test movement to right
+        testSpaceshipMove(KeyCode.RIGHT, Spaceship.DEFAULT_X_POS +
+                mySpaceship.X_SPEED_ON_KEY_PRESS, Spaceship.DEFAULT_X_POS);
+        // test movement to left
+        testSpaceshipMove(KeyCode.LEFT, Spaceship.DEFAULT_X_POS -
+                mySpaceship.X_SPEED_ON_KEY_PRESS, Spaceship.DEFAULT_X_POS);
     }
 
     @Test
     public void testSpaceshipWrap () {
-        // set position to far right of screen
-        mySpaceship.setX(Game.GAME_WIDTH - Spaceship.WIDTH);
-        // move it right one step by "pressing" the right arrow
-        sleep(1, TimeUnit.SECONDS);    // PAUSE: not typically recommended in tests
-        press(myScene, KeyCode.RIGHT);
-        sleep(1, TimeUnit.SECONDS);    // PAUSE: but useful when debugging to verify what is happening
-        // then check its position has changed properly
-        assertEquals(0, mySpaceship.getX());
-
-        // set position to far left of screen
-        mySpaceship.setX(0);
-        // move it left one step by "pressing" the left arrow
-        sleep(1, TimeUnit.SECONDS);    // PAUSE: not typically recommended in tests
-        press(myScene, KeyCode.LEFT);
-        sleep(1, TimeUnit.SECONDS);    // PAUSE: but useful when debugging to verify what is happening
-        // then check its position has changed properly
-        assertEquals(Game.GAME_WIDTH - mySpaceship.getFitWidth(), mySpaceship.getX());
+        // test wrap from right to left side of screen
+       testSpaceshipMove(KeyCode.RIGHT, 0, Game.GAME_WIDTH - Spaceship.WIDTH);
+       // test wrap from left to right side of screen
+       testSpaceshipMove(KeyCode.LEFT, Game.GAME_WIDTH - mySpaceship.WIDTH, 0);
     }
 
     @Test
     public void testEnemiesInitialPosition() {
         int rows = 4;
         double yPos = Game.GAME_HEIGHT/2.0 - Enemy.HEIGHT*rows/2.0;
-        for (int i = 0; i < rows; i++) {
+        for (int row = 0; row < rows; row++) {
             double xPos = (Game.GAME_WIDTH - Level.ENEMIES_PER_ROW * (Level.ENEMY_SPACING + Enemy.WIDTH) - Level.ENEMY_SPACING)/2;
-            for (int j = 0; j < Level.ENEMIES_PER_ROW; j++) {
-                Enemy curEnemy = myEnemies.get(i).get(j);
-
+            for (int col = 0; col < Level.ENEMIES_PER_ROW; col++) {
+                Enemy curEnemy = myEnemies.get(row).get(col);
+                // check that enemy is in expected position
                 assertEquals(curEnemy.getX(), xPos);
                 assertEquals(curEnemy.getY(), yPos);
-
                 xPos += Enemy.WIDTH + Level.ENEMY_SPACING;
             }
             yPos += Enemy.HEIGHT;
         }
     }
 
+    private boolean isNodeInMyScene(Node node) {
+        return myGame.getRoot().getChildren().contains(node);
+    }
+
+    private void testSpaceshipMove(KeyCode code, double expectedPosition, double startingXPos) {
+        // set initial position
+        mySpaceship.setX(startingXPos);
+        // move spaceship left or right one step by "pressing" the key
+        press(myScene, code);
+        // then check its position has changed properly
+        assertEquals(expectedPosition, mySpaceship.getX());
+    }
 }
