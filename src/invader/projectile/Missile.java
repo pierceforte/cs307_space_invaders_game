84a5fb2da38ee9765b34a14c415ca8173c0017f5
Pierce Forte <phf7@duke.edu>
2020-02-16 00:28:04 -0500

    Changed missile file name

--- src/invader/projectile/Missile.java
+++ src/invader/projectile/Missile.java
@@ -7,7 +7,7 @@ import javafx.scene.image.Image;
 public class Missile extends Projectile{// extends Laser {
     public static final double WIDTH = 10;
     public static final double HEIGHT = 20;
-    public static final String BOMB_IMG_NAME = "bomb.png";
+    public static final String BOMB_IMG_NAME = "missile.png";
     public static final String TYPE = "Missile";
     public static final int DAMAGE = 3;
 
